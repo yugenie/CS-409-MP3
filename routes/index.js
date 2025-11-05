559785ae45227622ebd0ce4866fdf211db9a607a
聂雨歌 <nieyuge@utas-MacBook-Pro.local>
2025-11-04 22:22:25 -0600

    connect endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@
  */
 module.exports = function (app, router) {
     app.use('/api', require('./home.js')(router));
+    app.use('/api/users', require('./users.js')(router));
+    app.use('/api/tasks', require('./tasks.js')(router));
 };
