597fd216311e013d78162666dbf90a4b0b03e032
聂雨歌 <nieyuge@utas-MacBook-Pro.local>
2025-11-04 18:52:16 -0600

    models - user and task schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ var mongoose = require('mongoose');
 
 // Define our user schema
 var UserSchema = new mongoose.Schema({
-    name: String
+    // name: String
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    pendingTasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+    dateCreated: { type: Date, default: Date.now },
 });
 
 // Export the Mongoose model
